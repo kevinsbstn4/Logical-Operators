1ca986a8bb53ce5a60042f10b95694248cde96f3
kevinsbstn4 <kevinsbstn4@gmail.com>
2019-01-15 08:39:24 +0700

    Logical Operators

---
+++ index.js
@@ -0,0 +1,8 @@
+let highIncome = false;
+let goodCreditScore = false;
+let eligibleForLoan = highIncome || goodCreditScore;
+console.log('Eligible' ,eligibleForLoan);
+
+
+let applicationRefused = !eligibleForLoan;
+console.log('Application Refused',applicationRefused);
